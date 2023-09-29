fca1a8770f2483d719d8050bf6f102395e9f33f0
Ricardo Pereira <patresio@gmail.com>
2023-09-29 10:31:36 -0300

    Primeiro Deploy OK

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventex.settings')
 
 application = Cling(get_wsgi_application())
+
+app = application
