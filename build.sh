c876b203ed3a521b99624f771b0bf19fcdf986e2
Ricardo Pereira <patresio@gmail.com>
2023-09-29 12:48:00 -0300

    Arrumando a bagunça

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 # Create a virtual environment
 echo "Creating a virtual environment..."
-python3.9 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 # Install the latest version of pip
