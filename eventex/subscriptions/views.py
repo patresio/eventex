7290025dae160ff5a085623f8bb60a1c04079520
Ricardo Pereira <patresio@gmail.com>
2023-10-02 14:16:34 -0300

    Habilita inscrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -12,14 +12,13 @@ from eventex.subscriptions.forms import SubscriptionForm
 def subscribe(request):
     if request.method == 'POST':
         form = SubscriptionForm(request.POST)
-        
-        if form.is_valid():
-            body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
-            mail.send_mail('Confirmação de inscrição', body, 'contato@eventex.com.br', ['contato@eventex.com.br', form.cleaned_data['email']])
-            messages.success(request, 'Inscrição realizada com sucesso!')
-            return HttpResponseRedirect('/inscricao/')
-        else:
+
+        if not form.is_valid():
             return render(request, 'subscriptions/subscription_form.html', {'form': form})
-        
+
+        body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+        mail.send_mail('Confirmação de inscrição', body, 'contato@eventex.com.br', ['contato@eventex.com.br', form.cleaned_data['email']])
+        messages.success(request, 'Inscrição realizada com sucesso!')
+        return HttpResponseRedirect('/inscricao/')
     context = {'form': SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', context)
