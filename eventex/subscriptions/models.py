0c0b36cd2bd96cc4c5308745a3976ac2b43b05eb
Ricardo Pereira <patresio@gmail.com>
2023-10-05 13:03:19 -0300

    Refatora Forms

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,13 +2,15 @@ import uuid
 
 from django.db import models
 
+from eventex.validators import validate_cpf
+
 
 # Create your models here.
 class Subscription(models.Model):
     name = models.CharField('Nome', max_length=100)
-    cpf = models.CharField('CPF', max_length=11)
-    email = models.EmailField('E-mail')
-    phone = models.CharField('Telefone', max_length=20)
+    cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
+    email = models.EmailField('E-mail', blank=True)
+    phone = models.CharField('Telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now=True)
     paid = models.BooleanField('Pago?', default=False)
 
