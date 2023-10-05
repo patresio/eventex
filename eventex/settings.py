724f1348c240b14213bda01cf347a3dba597e727
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2023-10-05 20:28:59 -0300

    PlanetScale

--- eventex/settings.py
+++ eventex/settings.py
@@ -81,11 +81,22 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
-default_dburl = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
-DATABASES = {
-    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
-}
+if DEBUG:
+    default_dburl = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+    DATABASES = {
+        'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+    }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('PLANET_DB'),
+        'HOST': config('PLANET_DB_HOST'),
+        'USER': config('PLANETSCALE_DB_USERNAME'),
+        'PASSWORD': config('PLANETSCALE_DB_PASSWORD'),
+        'OPTIONS': {'ssl': {'ca': config('PLANETSCALE_SSL_CERT_PATH')}}
+    }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
