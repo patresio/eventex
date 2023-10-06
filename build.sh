be36e607a933907e3dd58f75465c49ce061c9da3
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2023-10-05 22:24:25 -0300

    PlanetScale - MySQL Prisma - 556

--- build.sh
+++ build.sh
@@ -7,25 +7,25 @@
 
 # Install the latest version of pip
 echo "Installing the latest version of pip..."
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # Upgrade SetupTools
-python -m pip install --upgrade setuptools
+python3 -m pip install --upgrade setuptools
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 #export MYSQLCLIENT_CFLAGS=`pkg-config mysqlclient --cflags`
 #export MYSQLCLIENT_LDFLAGS=`pkg-config mysqlclient --libs`
 
 # Make migrations
 echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 # Carrega Keynotes
-python manage.py loaddata keynotes.json
+python3 manage.py loaddata keynotes.json
