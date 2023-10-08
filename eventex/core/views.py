6442f65e95cb69173951a12e5135bd744508e632
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2023-10-08 10:25:10 -0300

    Talk

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from eventex.core.models import Speaker
+from eventex.core.models import Speaker, Talk
 
 
 # Create your views here.
@@ -12,3 +12,11 @@ def home(request):
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
     return render(request, 'core/speaker_detail.html', {'speaker': speaker})
+
+
+def talk_list(request):
+    context = {
+        'morning_talks': Talk.objects.filter(start__lt='12:00'),
+        'afternoon_talks': Talk.objects.filter(start__gte='12:00')
+    }
+    return render(request, 'core/talk_list.html', context)
