ee6eb793450e8024fd331c04386e9899f5c88399
Ricardo Pereira <patresio@gmail.com>
2023-10-10 15:02:00 -0300

    Refatorando o codigo 3

--- eventex/settings.py
+++ eventex/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = str(BASE_DIR / 'staticfiles/static')
+STATIC_ROOT = str(BASE_DIR / 'staticfiles/static')
 STATICFILES_DIR = [BASE_DIR / 'static']
 
 #MEDIA_URL = 'img/'
