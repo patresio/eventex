c0859ded74bff10c75ca00eb1d0cf4ff545153a4
Ricardo Pereira <patresio@gmail.com>
2023-10-11 15:36:35 -0300

    Teste

--- eventex/settings.py
+++ eventex/settings.py
@@ -34,14 +34,12 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
-    'cloudinary_storage',
-
     'django.contrib.staticfiles',
     # Apps Thirds
     'test_without_migrations',
@@ -147,23 +145,21 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUD_NAME'),
     'API_KEY': config('CLOUD_API_KEY'),
     'API_SECRET': config('CLOUD_API_SECRET'),
-    'STATIC_TAG': 'static',
-    'STATICFILES_MANIFEST_ROOT': [BASE_DIR / 'manifest']
+    'STATICFILES_MANIFEST_ROOT': str(BASE_DIR / 'manifest/')
 }
 
 
 
-STATICFILES_DIR = [BASE_DIR / 'static']
-
 STATIC_URL = 'static/'
-STATIC_ROOT = str(BASE_DIR / 'static')
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+#STATIC_ROOT = str(BASE_DIR / 'staticfiles/static/')
+STORAGES = {
+    "default": {"BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage"},
+    "staticfiles": {"BACKEND": "cloudinary_storage.storage.StaticHashedCloudinaryStorage"}
+}
+
 
 MEDIA_URL = 'img/'
 MEDIA_ROOT = str(BASE_DIR / 'media/')
-
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
